bd918ce753abdc4aeeb48eb2240499a19e8b4d04
Tristan Simon <simontristandev@gmail.com>
2026-06-10 23:01:51 +0200

    new update to finder

--- TD_GetPidFromProcName.cpp
+++ TD_GetPidFromProcName.cpp
@@ -3,7 +3,22 @@
 DWORD TD_GetPidFromProcName(const wchar_t* ProcName) {
     DWORD PID = 0;
 
-    std::cout << "test" << std::endl;
+    HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    PROCESSENTRY32W PE;
+    PE.dwSize = sizeof(PE);
+    
+    if (hSnap == INVALID_HANDLE_VALUE) return 0;
+
+    if(Process32FirstW(hSnap, &PE)) {
+        do {
+            if (_wcsicmp(ProcName, PE.szExeFile) == 0) {
+                PID = PE.th32ProcessID;
+                break;
+            }
+        } while (Process32NextW(hSnap, &PE));
+    } 
+
+    std::cout << PID << std::endl;
 
     return PID;
 }
