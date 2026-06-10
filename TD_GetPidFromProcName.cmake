f423b91f45b0cd05c453aac2d9cb64e617fa0e8f
Tristan Simon <simontristandev@gmail.com>
2026-06-10 22:20:34 +0200

    fixed typo

--- TD_GetPidFromProcName.cmake
+++ TD_GetPidFromProcName.cmake
@@ -1,7 +1,7 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
 set(TD_GetPidFromProcName 
-    ${XOPTIONSWIDGET_SOURCES}
+    ${TD_GetPidFromProcName}
     ${CMAKE_CURRENT_LIST_DIR}/TD_GetPidFromProcName.cpp
     ${CMAKE_CURRENT_LIST_DIR}/TD_GetPidFromProcName.h
 )
